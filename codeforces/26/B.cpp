d68be19bdb35b19624475ce3a8af71153cef5b49
Soroush Sahraei <soroush.sahraei@gmail.com>
2019-07-19 20:21:00 +0430

    Add solution for problem `B - Regular Bracket Sequence` Link: https://codeforces.com/contest/26/problem/B Tags: greedy, *1400 Ref: https://codeforces.com/contest/26/submission/57335329

--- codeforces/26/B.cpp
+++ codeforces/26/B.cpp
@@ -25,7 +25,7 @@ int main(){
 	cout << cnt*2;
 	return(0);
 }
-//
+
 //When we hit our lowest point, we are open to the greatest change.
 //If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
 //Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
