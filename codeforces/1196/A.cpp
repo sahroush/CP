acc935465421ec22d96a39a54aa02f83bf7ef062
Soroush Sahraei <soroush.sahraei@gmail.com>
2019-07-24 22:12:00 +0430

    Add solution for problem `A - Three Piles of Candies` Link: https://codeforces.com/contest/1196/problem/A Tags: brute force, constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1196/submission/57700495

--- codeforces/1196/A.cpp
+++ codeforces/1196/A.cpp
@@ -5,6 +5,10 @@ using namespace std;
 typedef long long ll;
 //const ll MAX1 = 1e5+10;
 #define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+
+
+
 int main(){
 	ll q,l[3];
 	cin >>q;
@@ -17,6 +21,7 @@ int main(){
 	}
 	return(0);
 }
+
 //When we hit our lowest point, we are open to the greatest change.
 //If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
 //Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
