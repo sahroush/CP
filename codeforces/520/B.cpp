00e5d9074d50751c49d15e19bf767337fc7e54b6
Soroush Sahraei <soroush.sahraei@gmail.com>
2019-11-09 19:44:00 +0330

    Add solution for problem `B - Two Buttons` Link: https://codeforces.com/contest/520/problem/B Tags: dfs and similar, graphs, greedy, implementation, math, shortest paths, *1400 Ref: https://codeforces.com/contest/520/submission/64594832

---
+++ codeforces/520/B.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+
+const ll maxn = 1e6+100;
+const ld SADAT = 7.84e-17;
+const ll INF = 1e9;
+const ll mod = 1e9+7;
+const ld PI = 4 * atan((ld) 1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+
+ll pw(ll a, ll b) {
+    return (!b ? 1 : (b & 1 ? a * pw(a * a % mod, b / 2) % mod : pw(a * a % mod, b / 2) % mod));
+}
+
+int n , m;
+bool mark[maxn];
+int dist[maxn];
+
+queue <int> q;
+
+int main (){
+    migmig
+    cin >> n >> m;
+    if (n >= m){
+        dokme(n - m);
+    }
+    dist[n] = 0;
+    q.push(n);
+    mark[n] = 1;
+    while(!q.empty()){
+        int v = q.front();
+        q.pop();
+        if (!mark[v-1] and v-1 > 0){
+            mark[v-1] = 1;
+            q.push(v-1);
+            dist[v-1] = dist[v] + 1;
+            if (v-1 == m){
+                
+                dokme(dist[v-1]);
+            }
+        }
+        if (!mark[v*2] and v*2 < 1e5){
+            mark[v*2] = 1;
+            q.push(v*2);
+            dist[v*2] = dist[v] + 1;
+            if (v*2 == m){
+                dokme(dist[v*2]);
+            }
+        }
+
+    }
+
+    return(0);
+}
+
+
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you wiint come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
