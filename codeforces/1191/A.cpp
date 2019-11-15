aa4c684217148bf259478833bad74f7f355509aa
Soroush Sahraei <soroush.sahraei@gmail.com>
2019-11-15 16:11:00 +0330

    Add solution for problem `A - Tokitsukaze and Enhancement` Link: https://codeforces.com/contest/1191/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/1191/submission/65038828

---
+++ codeforces/1191/A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+
+const ll maxn =1e6+10;
+const ld SADAT = 7.84e-17;
+const ll INF = 1e9;
+const ll mod = 1e9+7;
+const ld PI = 4 * atan((ld) 1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+
+ll pw(ll a, ll b) {
+    return (!b ? 1 : (b & 1 ? a * pw(a * a % mod, b / 2) % mod : pw(a * a % mod, b / 2) % mod));
+}
+
+int n ;
+
+int main (){
+    migmig
+    cin >> n;
+    if (n%4 == 0){
+        cout << 1 << ' ' << 'A';
+    }
+    if(n%4 == 1){
+        cout << 0 << ' ' << 'A';
+    }
+    if (n%4 == 2){
+        cout << 1 << ' ' << 'B';
+    }
+    if(n%4 == 3){
+        cout << 2 << ' ' << 'A';
+    }
+
+
+
+
+
+    return(0);
+}
+
+
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you wiint come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
