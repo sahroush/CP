cc194bb68cad77cc7028a005f099177fb6088def
Soroush Sahraei <soroush.sahraei@gmail.com>
2019-11-23 21:04:00 +0330

    Add solution for problem `H - A + B Strikes Back` Link: https://codeforces.com/contest/409/problem/H Tags: *special problem, brute force, constructive algorithms, dsu, implementation, *1500 Ref: https://codeforces.com/contest/409/submission/65600009

--- codeforces/409/H.cpp
+++ codeforces/409/H.cpp
@@ -6,7 +6,7 @@ typedef long long ll;
 typedef long double ld;
 typedef pair<int ,int > pii;
 
-const ll maxn =2e5+10;
+const ll maxn =210000;
 const ld SADAT = 7.84e-17;
 const ll INF = 1e9;
 const ll mod = 1e9+7;
@@ -17,61 +17,24 @@ const ld PI = 4 * atan((ld) 1);
 #define dokme(x) cout << x ;  return(0);
 #define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
-#define random_init mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 ll pw(ll a, ll b) {
     return (!b ? 1 : (b & 1 ? a * pw(a * a % mod, b / 2) % mod : pw(a * a % mod, b / 2) % mod));
 }
 
-int par[maxn];
-vector <int> comp[maxn];
-int a , b;
+int n , q;
 
-int getpar(int v){
-    if(par[v] == v){
-        return(v);
-    }
-    par[v] = getpar(par[v]);
-    return(par[v]);
-}
-void merge(int x , int y){
-    if(getpar(x) == getpar(y)){
-        return;
-    }
-    x = getpar(x) , y = getpar(y);
-    if(comp[y].size() > comp[x].size()){
-        swap(x , y);
-    }
-    while(!comp[y].empty()){
-        int u = comp[y][comp[y].size() - 1];
-        comp[y].pop_back();
-        comp[x].pb(u);
-        par[u] = x;
-    }
-}
 
 int main(){
     migmig
-    cin >> a >> b;
-    int cnt = 0;
-    for (int i = 0 ; i < a ; i ++){
-        comp[i].pb(i);
-        par[i] = i;
-    }
-    for (int i = 0 ; i < b ; i ++ , cnt ++){
-        comp[cnt].pb(cnt);
-        par[cnt] = cnt;
-    }
-    for (int i = 0 ; i < a ;  i ++){
-        merge(0 , i);
-    }
-    for (int i = 0 , j = a ; i < b ; i ++ , j++){
-        merge(a , j);
-    }
-    a-- , b--;
-    merge(a , b);
-    cout << comp[par[a]].size();
-
+    cin >> n >> q;
+    cout << n + q;
+    //try 1
+    //try 2
+    //try 3
+    //try 4
+    //try 5
+    //try 6
 
 
     return(0);
