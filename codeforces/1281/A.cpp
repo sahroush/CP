d8257e58584ab398117e8100e9ebfe84d64cd921
Soroush Sahraei <soroush.sahraei@gmail.com>
2019-12-15 08:39:00 +0330

    Add solution for problem `A - Suffix Three` Link: https://codeforces.com/contest/1281/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1281/submission/66901008

---
+++ codeforces/1281/A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+const ll maxn =2e6;
+const ld SADAT = 7.84e-17;
+const ll INF = 1e9;
+const ll mod = 1e9+7;
+const ld PI = 4 * atan((ld) 1);
+
+#define mkp make_pair
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+#define random_init mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define ms(x , y) memset(x , y , sizeof x);
+#define prnt(x)  for(int i = 0 ; i < x.size() ; i ++) cout << x[i] << ' ';
+template<class T>bool checkmin(T &a,T b){return a > b ? a = b, 1 : 0;}
+ll pw(ll a, ll b) {
+    return (!b ? 1 : (b & 1 ? a * pw(a * a % mod, b / 2) % mod : pw(a * a % mod, b / 2) % mod));
+}
+
+int q;
+
+int main(){
+    migmig
+    cin >> q;
+    while(q -- ){
+        string s;
+        cin >> s;
+        string f = "" , j = "" , k = "";
+        int n = s.size() - 1;
+        f+=s[n - 1];
+        f += s[n];
+        k+=s[n - 4];
+        k+=s[n - 3];
+        k+=s[n - 2];
+        k+=s[n - 1];
+        k += s[n];
+        if(f == "po"){
+            cout << "FILIPINO" << endl;
+            continue;
+        }
+        if(k == "mnida"){
+            cout << "KOREAN" << endl;
+            continue;
+        }
+        cout << "JAPANESE" << endl;
+    }
+
+
+    return(0);
+}
+
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
