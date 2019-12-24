55a8661053d40b9f8e73549b434eb5c0e7d2a846
Soroush Sahraei <soroush.sahraei@gmail.com>
2019-12-24 18:43:00 +0330

    Add solution for problem `B1 - K for the Price of One (Easy Version)` Link: https://codeforces.com/contest/1282/problem/B1 Tags: dp, greedy, sortings, *1400 Ref: https://codeforces.com/contest/1282/submission/67537317

---
+++ codeforces/1282/B1.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+const ll maxn =4e6;
+const ld SADAT = 7.84e-17;
+const ll INF = 1e9;
+const ll mod = 1e9+7;
+const ld PI = 4 * atan((ld) 1);
+
+#define mkp make_pair
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+#define random_init mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define ms(x , y) memset(x , y , sizeof x);
+#define prnt(x)  for(int i = 0 ; i < x.size() ; i ++) cout << x[i] << ' ';
+ll pw(ll a, ll b) {
+    return (!b ? 1 : (b & 1 ? a * pw(a * a % mod, b / 2) % mod : pw(a * a % mod, b / 2) % mod));
+}
+
+int qu;
+int a[maxn];
+int n , p , k;
+queue <int> q;
+ll sum = 0;
+
+
+int main(){
+    migmig
+    cin >> qu;
+    while (qu -- ){
+        cin >> n >> p >> k;
+        for (int i  =0 ; i < n ; i ++){
+            cin >> a[i];
+        }
+        sort(a , a + n);
+        if(p < a[0]){
+            cout << 0 << endl;
+            continue;
+        }
+        int mx = 0;
+        while(!q.empty()){
+            q.pop();
+        }
+        sum = 0;
+        int pos = 0;
+        int cnt = 0;
+        bool f = 0;
+        while(pos < n){
+            q.push(pos);
+            cnt ++;
+            sum+=a[pos];
+            if(f == 0 ){
+                f = 1;
+            }
+            else if(f == 1){
+                sum -= a[pos - 1];
+                //cout << a[pos - 1] << ' ';
+                f = 0;
+            }
+            if(sum<=p){
+                mx = max(mx , (int)q.size());
+            }
+            else{
+                sum+=a[0];
+                sum-=a[pos - 1];
+                sum+=a[pos];
+                //dokme(pos);
+                if(sum<=p){
+                    mx = max(mx , (int)q.size() + 1);
+                }
+                break;
+            }
+            pos ++;
+        }
+        while(!q.empty()){
+            q.pop();
+        }
+        sum = a[0];
+         pos = 1;
+         cnt = 1;
+         f = 0;
+        int mx2 = 1;
+        while(pos < n){
+            q.push(pos);
+            cnt ++;
+            sum+=a[pos];
+            if(f == 0 ){
+                f = 1;
+            }
+            else if(f == 1){
+                sum -= a[pos - 1];
+                //cout << a[pos - 1] << ' ';
+                f = 0;
+            }
+            if(sum<=p){
+                mx2 = max(mx2 , (int)q.size()+1);
+            }
+            else{
+                sum+=a[0];
+                sum-=a[pos - 1];
+                sum+=a[pos];
+                //dokme(pos);
+                
+                break;
+            }
+            pos ++;
+        }
+        cout << max(mx , mx2) << endl;
+
+
+    }
+
+
+    return(0);
+}
+
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
