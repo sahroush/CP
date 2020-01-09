d4fe029bc4eefe92e5de3c994feeeb4a318e6077
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-01-09 14:30:00 +0330

    Add solution for problem `E - Playing on Graph` Link: https://codeforces.com/contest/542/problem/E Tags: graphs, shortest paths, *2600 Ref: https://codeforces.com/contest/542/submission/68441187

--- codeforces/542/E.cpp
+++ codeforces/542/E.cpp
@@ -122,3 +122,10 @@ int main(){
 	
     return(0);
 }
+
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
+
