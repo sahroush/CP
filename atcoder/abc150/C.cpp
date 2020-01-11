c0d5e719ee5b52cd96c323fb02122d23f0257fb3
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-01-11 22:25:00 +0330

    Add solution for problem `C - Count Order` Link: https://atcoder.jp/contests/abc150/tasks/abc150_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc150/submissions/9430547

--- atcoder/abc150/C.cpp
+++ atcoder/abc150/C.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 typedef long long ll;
 typedef long double ld;
 typedef pair<int  ,int > pii;
 typedef pair<ll  ,ll > pll;
- 
+
 const ll maxn =5e5;
 const ld SADAT = 7.84e-17;
 const ll INF = 1e18;
 const ll mod = 1e9+7;
 const ld PI = 4 * atan((ld) 1);
- 
+
 #define pb(x) push_back(x);
 #define endl '\n'
 #define dokme(x) cout << x ;  return(0);
@@ -25,28 +25,28 @@ const ld PI = 4 * atan((ld) 1);
 ll pw(ll a, ll b) {
     return (!b ? 1 : (b & 1 ? a * pw(a * a % mod, b / 2) % mod : pw(a * a % mod, b / 2) % mod));
 }
- 
+
 int n;
 int p[10] , q[10] , ans[10];
- 
+
 bool ok(int x[]){
     for (int i  = 1 ; i <= n ; i ++){
         if(ans[i]!=x[i]) return(0);
     }
     return(1);
 }
- 
+
 int main(){
     migmig
     cin >> n;
     for (int i  = 1 ; i <= n ; i  ++){
         cin >> p[i];
     }
- 
+
     for (int i  = 1 ; i <= n ; i  ++){
         cin >> q[i];
     }
- 
+
     for (int i  = 1 ; i <= n ; i  ++){
         ans[i]  =i;
     }
@@ -66,8 +66,8 @@ int main(){
     }
     return(0);
 }
- 
- 
+
+
 //When we hit our lowest point, we are open to the greatest change.
 //If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
 //Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
