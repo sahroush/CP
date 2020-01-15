6794b6f8e7e6fcfd1f98af04d1171c0957c9cbaf
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-01-15 19:59:00 +0330

    Add solution for problem `C - Mahmoud and Ehab and the xor` Link: https://codeforces.com/contest/862/problem/C Tags: constructive algorithms, *1900 Ref: https://codeforces.com/contest/862/submission/68874721

---
+++ codeforces/862/C.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+typedef pair<ll  ,ll > pll;
+
+const ll maxn =2e6;
+const ll mod = 1e9+7;
+const ld PI = 4 * atan((ld) 1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+#define ms(x , y) memset(x , y , sizeof x);
+ll pw(ll a, ll b) {
+    return (!b ? 1 : (b & 1 ? a * pw(a * a % mod, b / 2) % mod : pw(a * a % mod, b / 2) % mod));
+}
+
+int n , inp;
+ll bit[maxn] , x;
+
+int main(){
+    migmig
+    cin >> n >> inp;
+    x = inp;
+    if(x == 0 and n == 2){
+        dokme("NO");
+    }
+    cout << "YES" << endl;
+    if(n == 1){
+        dokme(inp);
+    }
+    if(n == 2){
+        cout << inp << ' ' << 0;
+        return(0);
+    }
+    ll sum = 0;
+    for(int i = 1 ; i <= n - 3 ; i ++){
+        sum = sum ^ i;
+        cout << i << ' ';
+    }
+    if(x == sum){
+        ll boz = pw(2 , 18);
+        ll moz = pw(2 , 19);
+        boz = boz^moz;
+        cout << pw(2 , 18) << ' ' << pw(2 , 19) << ' ' << boz;
+        return(0);
+    }
+    ll boz = pw(2 , 18);
+    ll moz = (boz^sum);
+    moz = moz^x;
+    cout << 0 << ' ' << boz << ' ' << moz;
+
+    return(0);
+}
+
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
