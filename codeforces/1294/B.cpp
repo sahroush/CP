174b54c72dbea28dc02fd56ad33f170b1a7f1e54
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-01-22 18:15:00 +0330

    Add solution for problem `B - Collecting Packages` Link: https://codeforces.com/contest/1294/problem/B Tags: implementation, sortings, *1200 Ref: https://codeforces.com/contest/1294/submission/69311834

---
+++ codeforces/1294/B.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+typedef pair<ll  ,ll > pll;
+
+const ll maxn =2e6;
+const ll mod = 1e9+7;
+const ld PI = 4 * atan((ld) 1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+#define ms(x , y) memset(x , y , sizeof x);
+ll pw(ll a, ll b) {
+    return (!b ? 1 : (b & 1 ? a * pw(a * a % mod, b / 2) % mod : pw(a * a % mod, b / 2) % mod));
+}
+
+int q;
+pii pos[maxn];
+
+int main(){
+    migmig
+    cin >> q;
+    while(q--){
+        int n;
+        cin >> n;
+        for (int i = 0 ; i < n ; i ++){
+            cin >> pos[i].first;
+            cin >> pos[i].second;
+        }
+        sort(pos , pos + n);
+        bool ter = 0;
+        for (int i = 1 ; i< n ; i ++){
+            if(pos[i].second < pos[i - 1].second){
+                cout << "NO" << endl;
+                ter =1;
+                break;
+            }
+        }
+        if(ter)continue;
+        cout << "YES" << endl;
+        pii p = pii(0 , 0);
+        for (int i = 0 ; i < n ; i ++){
+            for (int j = p.first ; j < pos[i].first ; j ++){
+                cout << 'R';
+            }
+            p.first = pos[i].first;
+            for (int j = p.second ; j < pos[i].second ; j ++){
+                cout << 'U';
+            }
+            p.second = pos[i].second;
+        }
+        cout << endl;
+    }
+
+
+
+    return(0);
+}
+
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
