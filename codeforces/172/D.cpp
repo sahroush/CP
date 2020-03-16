b286846a62823f3b044fd2a4a43ba177fe1da4f3
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-03-16 20:32:00 +0330

    Add solution for problem `D - Calendar Reform` Link: https://codeforces.com/contest/172/problem/D Tags: *special problem, number theory, *1500 Ref: https://codeforces.com/contest/172/submission/73426265

---
+++ codeforces/172/D.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+typedef pair<ll  ,ll > pll;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn =1e7+10;
+const ll mod = 1e9+7;//998244353;
+const ld PI = 4 * atan((ld) 1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dominance true
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+#define ms(x , y) memset(x , y , sizeof x);
+ll pw(ll a, ll b, ll md) {
+    return (!b ? 1 : (b & 1 ? a * pw(a * a % md, b / 2, md) % md : pw(a * a % md, b / 2, md) % md));
+}
+
+ll a , n;
+bool mark[maxn];
+ll val[maxn];
+
+int main(){
+    //migmig
+    fill(val , val+maxn , 1);
+    cin >> a >> n;
+    // val harki mishe bishtarin morabesh
+    for (ll i = 2 ; i * i < maxn ; i ++){
+        if(!mark[i]){
+            for (ll j = i ; j < maxn ; j += i){
+                mark[j] = 1;
+            }
+            for (ll j = i*i ; j < maxn ; j *= (i * i)){
+                val[j]*=(i*i);
+                for (int k = j + j ; k < maxn ; k += j){
+                    val[k]*=(i*i);
+                }
+            }
+        }
+    }
+    ll sum = 0;
+    for (int i = 0 ; i < n ; i ++){
+        sum += (a + i ) / val[a + i];
+    }
+    cout << sum;
+    return(0);
+}
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
+//kuxtal jach uts chuchul u u páajtal máak jach táaj meyajilo'ob le castigadas tumen ch'aik descansos.
