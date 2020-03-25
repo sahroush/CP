f84860f94ac69cc0351ed0f3ba2e687fd10e761d
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-03-25 21:36:00 +0430

    Add solution for problem `D - Safe` Link: https://codeforces.com/contest/47/problem/D Tags: brute force, *2200 Ref: https://codeforces.com/contest/47/submission/74317557

---
+++ codeforces/47/D.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+typedef pair<ll  ,ll > pll;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = 4 * atan((ld) 1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dominance true
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+#define ms(x , y) memset(x , y , sizeof x);
+ll pw(ll a, ll b, ll md = mod) {
+    ll res = 1;while(b){if(b&1){res=(a*res)%mod;}a=(a*a)%mod;b>>=1;}return(res);
+}
+
+int n , m;
+string num[maxn];
+int ac[maxn];
+int cnt = 0;
+bitset < 40 > val[maxn] , cur , ans[maxn];
+
+void solve(int x = ac[0], int base = n){
+    if(base == 0){
+        ans[cnt++] = cur;
+        return;
+    }
+    cur[base - 1] = 1;
+    if(x > 0){
+        solve(x - 1 , base - 1);
+    }
+    cur[base - 1] = 0;
+    if(x < base){
+        solve(x , base - 1);
+    }
+}
+
+int main(){
+    migmig
+    cin >> n >> m;
+    for (int i = 0 ; i < m;  i ++){
+        cin >> num[i] >> ac[i];
+        for (int j = 0 ; j < n ; j ++){
+            val[i][j] = num[i][j]-'0';
+        }
+    }
+    solve();
+    int res = 0;
+    for (int i = 0 ; i < cnt ; i ++){
+        cur = val[0]^ans[i];
+        int pos = 0 ;
+        while(pos < m  and  (cur^val[pos]).count() == ac[pos]){
+            pos ++;
+        }
+        if(pos == m){
+            res++;
+        }
+    }
+    cout << res;
+    return(0);
+}
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
+//kuxtal jach uts chuchul u u páajtal máak jach táaj meyajilo'ob le castigadas tumen ch'aik descansos.
