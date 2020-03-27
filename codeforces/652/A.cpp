ab1212fe5ba2891ce2f5f4b3a5e0723920b40761
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-03-27 21:11:00 +0430

    Add solution for problem `A - Gabriel and Caterpillar` Link: https://codeforces.com/contest/652/problem/A Tags: implementation, math, *1400 Ref: https://codeforces.com/contest/652/submission/74580540

---
+++ codeforces/652/A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+typedef pair<ll  ,ll > pll;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dominance true
+#define dokme(x) cout << x ;  return(0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+#define ms(x , y) memset(x , y , sizeof x);
+ll pw(ll a, ll b, ll md = mod) {
+    ll res = 1;while(b){if(b&1){res=(a*res)%mod;}a=(a*a)%mod;b>>=1;}return(res);
+}
+
+int h1 , h2 , a1 , a2;
+
+int main(){
+    migmig
+    cin >> h1 >> h2 >> a1 >> a2;
+    int pre = h1;
+    h1+=a1*8;
+    if(h1>=h2){
+        dokme(0)
+    }
+    int cnt = 1;
+    while(h1 < h2){
+        h1-=a2*12;
+        h1+=a1*12;
+        if(h1>=h2){
+            break;
+        }
+        cnt++;
+        if(h1 < pre or cnt > 1e6){
+            dokme(-1);
+        }
+    }
+    cout << cnt;
+
+
+    return(0);
+}
+
+//When we hit our lowest point, we are open to the greatest change.
+//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
+//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
+//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
+//kuxtal jach uts chuchul u u páajtal máak jach táaj meyajilo'ob le castigadas tumen ch'aik descansos.
