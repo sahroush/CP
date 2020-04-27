da5bedbfe1445e7ff9a3ad1655f30dfe9c4bb0ac
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-04-27 16:21:00 +0430

    Add solution for problem `B - Cover Points` Link: https://codeforces.com/contest/1047/problem/B Tags: geometry, math, *900 Ref: https://codeforces.com/contest/1047/submission/78285761

---
+++ codeforces/1047/B.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb(x) push_back(x);
+#define endl '\n'
+#define dokme(x) return(cout << x , 0);
+#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ms(x , y) memset(x , y , sizeof x);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+ll ans = 0;
+int q;
+
+int main(){
+    migmig
+    cin >> q;
+    while(q -- ){
+		ll a , b;
+		cin >> a >> b;
+		ans = max(ans , a+b);
+	}
+    cout << ans;
+    
+    
+    
+    return(0);
+}
+
