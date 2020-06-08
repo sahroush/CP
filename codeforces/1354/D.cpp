29b74ae50978e5521717d25c440db4e1a8822876
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-06-08 22:13:00 +0430

    Add solution for problem `D - Multiset` Link: https://codeforces.com/contest/1354/problem/D Tags: binary search, data structures, *1900 Ref: https://codeforces.com/contest/1354/submission/83160987

--- codeforces/1354/D.cpp
+++ codeforces/1354/D.cpp
@@ -22,46 +22,33 @@ ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*
 
 int n , q;
 
-struct oset{
-    int maxn;
-    vector < int > fen;
-    oset(int n):
-        maxn(n+100),
-        fen(maxn){}
-        
-    void add(int x , int pos){
-        for( ; pos < maxn ; pos += pos & -pos)
-            fen[pos] += x;
+struct ofen{
+    int fen[maxn] , size = 0;
+    ofen(int x){
+        ms(fen , 0);
     }
-    int get(int pos){
-        int sum = 0 ;
-        for( ; pos ; pos -= pos & -pos)
-            sum += fen[pos];
-        return(sum);
+    void insert(int x){
+        size++;
+        for(;x < maxn;x+=x&(-x))
+            fen[x]++;
     }
-    
-    void insert(int x , int cnt = 1){
-        add(cnt , x);
+    void erase(int x){
+        size--;
+        for(;x<maxn;x+=x&(-x))
+            fen[x]--;
     }
-    void erase(int x , int cnt = 1){
-        add(-cnt , x);
-    }
-    
-    int find_by_order(int k){ // k-th element
-        int sum = 0 , pos = 0;
-        for(int i = log2(maxn) ; i >= 0 ; i --)
-            if(pos + (1 << i) < maxn and sum + fen[pos + (1 << i)] < k)
-                pos += (1 << i),
-                sum += fen[pos];
-        return(pos + 1);
-    }
-    int order_of_key(int key){ // number of elements <= key
-        return(get(key));
+    int val(int x){
+        int pos = 0;
+        int sum = 0;
+        for(int i = 20 ; i>=0 ; i --){
+            if(pos+(1 << i) < maxn and sum+fen[pos+(1 << i)] < x)
+                pos += (1 << i), sum += fen[pos];
+        }
+        return(pos+1);
     }
 };
 
-
-oset f(maxn);
+ofen f(0);
 
 int main(){
     migmig
@@ -75,12 +62,12 @@ int main(){
         int x;
         cin >> x;
         if(x < 0)
-            f.erase(f.find_by_order(-x));
+            f.erase(f.val(-x));
         else
             f.insert(x);
     }
-    if(f.get(maxn - 1) == 0)
+    if(f.size == 0)
         dokme(0);
-    dokme(f.find_by_order(1));
+    dokme(f.val(1));
     return(0);
 }
