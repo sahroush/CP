1a8f6e8c1401c5d9bd1740ceaf31abdb51981705
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-08-04 21:07:00 +0430

    Add solution for problem `E - Holes` Link: https://codeforces.com/contest/13/problem/E Tags: data structures, dsu, *2700 Ref: https://codeforces.com/contest/13/submission/88889334

--- codeforces/13/E.cpp
+++ codeforces/13/E.cpp
@@ -1,16 +1,38 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma,tune=native")
+//*/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int maxn  = 3e6;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
 #define endl '\n'
+#define dokme(x) return(cout << x , 0);
 #define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ms(x , y) memset(x , y , sizeof x);
+#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
 
-int n , q , sq,nxt[maxn],dist[maxn],last[maxn],comp[maxn],a[maxn];
+int n , q;
+int nxt[maxn];
+int dist[maxn];
+int last[maxn];
+int comp[maxn];
+int a[maxn];
 
 void solve(int i){
-    comp[i] = i/sq;if(a[i]+i > n)a[i] = n+1 - i;
     if(i + a[i] == n+1)
         nxt[i] = 0 , last[i] = i , dist[i] = 1;
     else if(comp[i] == comp[i+a[i]])
@@ -22,22 +44,33 @@ void solve(int i){
 int main(){
     migmig
     cin >> n >> q;
-    sq = sqrt(n);
-    for(int i = 1 ; i <= n; i ++)
+    int sq = sqrt(n);
+    for(int i = 1 ; i <= n; i ++){
         cin >> a[i];
+        if(i+a[i] > n) a[i] = n+1 - i;
+        comp[i] = i / sq;
+    }
     for(int i = n ; i ; i --)
         solve(i);
     while(q -- ){
         int t; cin >> t;
         if(t){
-            int cur , sum = 0 , lst = 0;cin >> cur;
-            while(cur!=0)
-                sum += dist[cur],lst = last[cur],cur = nxt[cur];
+            int cur;
+            int sum = 0;
+            int lst = 0;
+            cin >> cur;
+            while(cur!=0){
+                sum += dist[cur];
+                lst = last[cur];
+                cur = nxt[cur];
+            }
             cout << lst << ' ' << sum << endl;
         }
         else{
-            int pos , x; cin >> pos >> x;
-            if(x+pos > n) x = n+1 - pos;a[pos] = x;
+            int pos , x;
+            cin >> pos >> x;
+            if(x+pos > n) x = n+1 - pos;
+            a[pos] = x;
             for(int i = pos ; i and comp[i] == comp[pos] ; i --)
                 solve(i);
         }
