6cb595aa5d88a0e0b958cd5d204495dc3ca74ebd
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-10-15 20:25:00 +0330

    Add solution for problem `D - Multiset` Link: https://codeforces.com/contest/1354/problem/D Tags: binary search, data structures, *1900 Ref: https://codeforces.com/contest/1354/submission/95606879

--- codeforces/1354/D.cpp
+++ codeforces/1354/D.cpp
@@ -8,11 +8,11 @@ typedef pair<int  ,int > pii;
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const ll maxn  = 3e6;
+const ll maxn  = 1e6+100;
 const ll mod =1e9+7;
 const ld PI = acos((ld)-1);
 
-#define pb(x) push_back(x);
+#define pb push_back
 #define endl '\n'
 #define dokme(x) return(cout << x , 0);
 #define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
@@ -20,71 +20,67 @@ const ld PI = acos((ld)-1);
 #define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
 ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
 
-const ll N = 1e6+100;
+int n , q;
 
-struct indexed_set {
-	
-	int sz = 0, bit[N];
-
-	int size() {
-		return sz;
-	}
-
-	void update(int k, int x) {
-		while (k < N) {
-			bit[k] += x;
-			k += k & -k;
-		}
-		sz += x;
-	}
-
-	int find_by_order(int k) {
-		int ans = 0, sum = 0;
-		for (int j = 20; j >= 0; --j) {
-			ans += 1 << j;
-			if (ans < N && sum + bit[ans] < k) {
-				sum += bit[ans];
-			} else {
-				ans -= 1 << j;
-			}
-		}
-		return ans + 1;
-	}
-	
-	int order_of_key(int k) {
-		int ans = 0;
-		while (k >= 1) {
-			ans += bit[k];
-			k -= k & -k;
-		}
-		return ans;
-	}
-	
-} is;
+struct oset{
+    int maxn;
+    vector < int > fen;
+    oset(int n):
+        maxn(n+100),
+        fen(maxn){}
+        
+    void add(int x , int pos){
+        for( ; pos < maxn ; pos += pos & -pos)
+            fen[pos] += x;
+    }
+    int get(int pos){
+        int sum = 0 ;
+        for( ; pos ; pos -= pos & -pos)
+            sum += fen[pos];
+        return(sum);
+    }
+    
+    void insert(int x , int cnt = 1){
+        add(cnt , x);
+    }
+    void erase(int x , int cnt = 1){
+        add(-cnt , x);
+    }
+    
+    int find_by_order(int k){ // k-th element
+        int sum = 0 , pos = 0;
+        for(int i = log2(maxn) ; i >= 0 ; i --)
+            if(pos + (1 << i) < maxn and sum + fen[pos + (1 << i)] < k)
+                pos += (1 << i),
+                sum += fen[pos];
+        return(pos + 1);
+    }
+    int order_of_key(int key){ // number of elements <= key
+        return(get(key));
+    }
+};
 
 
-int n , t;
-int sz = 0;
-int q , a;
-
+oset f(maxn);
 
 int main(){
     migmig
-    cin >> n >> t;
-	sz = n;
-	for(int i = 0 ; i < n ; i ++){
-		cin >> a;
-		is.update(a , 1);
-	}
-	for(int i = 0 ; i < t ; i ++){
-		cin >> q;
-		if(q > 0)
-			is.update(q , 1) , sz++;
-		else
-			is.update(is.find_by_order(-q) , -1) , sz--;
-	}
-    if(sz == 0)
-		dokme(0);
-    cout << is.find_by_order(1);
+    cin >> n >> q;
+    for(int i = 0 ; i < n ; i ++){
+        int x;
+        cin >> x;
+        f.insert(x);
+    }
+    for(int i = 0 ; i < q ; i ++){
+        int x;
+        cin >> x;
+        if(x < 0)
+            f.erase(f.find_by_order(-x));
+        else
+            f.insert(x);
+    }
+    if(f.get(maxn - 1) == 0)
+        dokme(0);
+    dokme(f.find_by_order(1));
     return(0);
 }
