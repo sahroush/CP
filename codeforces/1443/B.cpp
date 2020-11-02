9cd7d7739fb2d8e0dfc4842552181f331c49c532
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-11-02 18:19:00 +0330

    Add solution for problem `B - Saving the City` Link: https://codeforces.com/contest/1443/problem/B Tags: dp, greedy, math, sortings, *1300 Ref: https://codeforces.com/contest/1443/submission/97443511

---
+++ codeforces/1443/B.cpp
@@ -0,0 +1,61 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma,tune=native")
+//*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+int q , n;
+ll a , b;
+string s;
+
+ll dp[maxn];
+
+int32_t main(){
+    migmig;
+	cin >> q;
+	while(q -- ){
+		cin >> a >> b;
+		cin >> s;
+		int n = s.size();
+		int lst = -1;
+		if(s[0] == '1')dp[0] = a , lst = 0;
+		else
+			dp[0] = 0;
+		for(int i = 1 ; i < n ; i ++){
+			if(s[i] == '0')continue;
+			if(lst == -1){
+				dp[i] = a;
+				lst = i;
+				continue;
+			}
+			dp[i] = min(dp[lst] + a , dp[lst] + b * (i -  lst - 1));
+			lst = i;
+		}
+		cout << dp[lst] << endl;
+	}
+
+
+
+
+    return(0);
+}
