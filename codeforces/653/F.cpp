ca28fee9fcb2aad121d8ab8233d66cd3b4b0ff2b
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-11-09 11:37:00 +0330

    Add solution for problem `F - Paper task` Link: https://codeforces.com/contest/653/problem/F Tags: data structures, string suffix structures, strings, *2600 Ref: https://codeforces.com/contest/653/submission/97972775

--- codeforces/653/F.cpp
+++ codeforces/653/F.cpp
@@ -1,198 +1,111 @@
 //*
 #pragma GCC optimize("O2")
 #pragma GCC optimize("Ofast")
-//#pragma GCC optimize("unroll-loops")
-//#pragma GCC target("avx,avx2,sse,sse2,fma,tune=native")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma,tune=native")
 //*/
 #include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
 
-const int maxn  = 5e5+10;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 5e5+10;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
 
 #define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
 #define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
 
-namespace suffix_array {
-template <class T>
-void induce(int n, const T s[], bool isL[], int sa[], int l_head[], int B) {
-    int buf[B + 2];
-    copy_n(l_head, B + 2, buf);
+int sa[maxn];
+int rk[maxn];
+int tp[maxn];
+int cnt[maxn];
+int lcp[maxn];
 
-    for (int i = 0; i < n; ++i) {
-        if (sa[i] > 0 && isL[sa[i] - 1]) {
-            int pos = (int)s[sa[i] - 1];
-            sa[buf[pos]++] = sa[i] - 1;
-        }
-    }
-
-    copy_n(l_head, B + 2, buf);
-
-    for (int i = n - 1; i >= 0; --i) {
-        if (sa[i] > 0 && !isL[sa[i] - 1]) {
-            int pos = (int)s[sa[i] - 1];
-            sa[--buf[pos + 1]] = sa[i] - 1;
-        }
-    }
+void SA(string &s){
+	int A = 'z' , p = 0 , n = s.size();
+	if(n == 1){
+		sa[0] = rk[0] = 0;
+		return;
+	}
+	for(int i = 0 ; i < n ; i ++)
+		sa[i] = i , rk[i] = s[i];
+	for(int j = 1 ; p < n - 1 ; j<<=1){
+		p = 0;
+		int k = (j>>1);
+		for(int i = n - k ; i < n ; i ++)
+			tp[p ++ ] = i;
+		for(int i = 0 ; i < n ; i ++)
+			if(sa[i] >= k)
+				tp[p ++] = sa[i] - k;
+		for(int i = 0 ; i <= A ; i ++)
+			cnt[i] = 0;
+		for(int i = 0 ; i < n ; i ++)
+			cnt[rk[i]] ++;
+		for(int i = 1 ; i <= A ; i ++)
+			cnt[i] += cnt[i - 1];
+		for(int i = n - 1 ; i >= 0 ; i --)
+			sa[--cnt[rk[tp[i]]]] = tp[i];
+		swap(rk , tp);
+		rk[sa[0]] = p = 0;
+		for(int i = 1 ; i < n ; i ++)
+			p += (tp[sa[i - 1]]!=tp[sa[i]] || sa[i - 1] + k  >= n ||
+			 tp[sa[i - 1]+k] != tp[sa[i] + k]) , rk[sa[i]] = p;
+		A = p;
+	}
 }
 
-// sentinel must be added to the end of s in advance
-
-template <class T>
-void SA_IS(int n, const T s[], int sa[], int B = 200) {
-    bool isL[n];
-
-    isL[n - 1] = false;
-
-    for (int i = n - 2; i >= 0; --i) {
-        isL[i] = (s[i] > s[i + 1] || (s[i] == s[i + 1] && isL[i + 1]));
-    }
-
-    int l_head[B + 2] = {0};
-
-    for (int i = 0; i < n; ++i) ++l_head[(int)s[i]];
-
-    int sum = 0;
-
-    for (int i = 0; i <= B + 1; ++i) {
-        l_head[i] += sum;
-        swap(sum, l_head[i]);
-    }
-
-    vector<int> lms;
-    int lms_id[n];
-
-    fill_n(lms_id, n, -1);
-
-    int buf[B + 2];
-    copy_n(l_head, B + 2, buf);
-
-    int now = 0;
-
-    fill_n(sa, n, -1);
-
-    for (int i = 1; i < n; ++i) {
-        if (isL[i - 1] && !isL[i]) {
-            lms_id[i] = now++;
-            lms.push_back(i);
-            sa[--buf[(int)s[i] + 1]] = i;
-        }
-    }
-
-    vector<int> lms2 = lms;
-    induce(n, s, isL, sa, l_head, B);
-
-    now = 0;
-
-    for (int i = 0; i < n; ++i) {
-        if (lms_id[sa[i]] != -1) {
-            lms[now++] = sa[i];
-        }
-    }
-
-    int s_lms[now], sa_lms[now + 1];
-
-    now = 0;
-    s_lms[lms_id[lms[0]]] = 0;
-
-    for (int i = 1; i < lms.size(); ++i) {
-        int u = lms[i - 1], v = lms[i];
-
-        bool diff = false;
-
-        for (int j = 0; j < n; ++j) {
-            if (s[u + j] != s[v + j] || isL[u + j] != isL[v + j]) {
-                diff = true;
-                break;
-            }
-
-            if (j > 0 && (lms_id[u + j] != -1 || lms_id[v + j] != -1)) break;
-        }
-
-        if (diff) now++;
-        s_lms[lms_id[lms[i]]] = now;
-    }
-
-    if (now + 1 != lms.size()) {
-        SA_IS(lms.size(), s_lms, sa_lms, now);
-    } else {
-        for (int i = 0; i < lms.size(); ++i) {
-            sa_lms[s_lms[i]] = i;
-        }
-    }
-
-    for (int i = 0; i < lms.size(); ++i) {
-        lms[i] = lms2[sa_lms[i]];
-    }
-
-    copy_n(l_head, B + 2, buf);
-
-    fill_n(sa, n, -1);
-
-    for (int i = (int)lms.size() - 1; i >= 0; --i) {
-        int p = s[lms[i]];
-        sa[--buf[p + 1]] = lms[i];
-    }
-
-    induce(n, s, isL, sa, l_head, B);
+void LCP(string &s){
+	for(int i = 0 , k = 0 ; i < s.size() ; i ++){
+		if(rk[i] == 0)continue;
+		if(k) k --;
+		while(s[i + k] == s[sa[rk[i] - 1] + k]) k ++;
+		lcp[rk[i]] = k;
+	}
 }
 
-template <class T>
-void LCP(int n, const T s[], const int sa[], int lcp[]) {
-    int rsa[n + 1];
-    for (int i = 0; i <= n; i++) {
-        rsa[sa[i]] = i;
-    }
+const int Log = 20;
 
-    int h = 0;
-    for (int i = 0; i < n; i++) {
-        int j = sa[rsa[i] - 1];
-
-        if (h > 0) h--;
-        for (; j + h < n && i + h < n; h++) {
-            if (s[j + h] != s[i + h]) break;
-        }
-        lcp[rsa[i] - 1] = h;
-    }
-}
-};  // namespace suffix_array
-char s[maxn];
+string s;
 int n;
-int a[maxn];
+int a[Log+2][maxn];
 vector < int > pos[2*maxn];
-int stk[maxn];
-int R[maxn];
 
-int sa[maxn] , lcp[maxn];
+int solve(int l , int r = n-1){
+	int cur = l;
+	for(int i = Log-1 ; i >= 0 ; i --){
+		if(cur + (1 << i) > r)continue;
+		if(a[i][cur] >= a[0][l]-1)cur += (1 << i);
+	}
+	auto &v = pos[a[0][l]-1 + maxn];
+	return(upper_bound(v.begin() , v.end() , cur) - 
+	 upper_bound(v.begin() , v.end() , l ) + 1);
+}
 
 int32_t main(){
     migmig;
     cin >> n;
-    cin >> s;
-    suffix_array::SA_IS(n + 1, s, sa);
-    suffix_array::LCP(n, s, sa, lcp);
-	a[n+1] = -1e9;
-	for(int i = 1 ; i <= n ; i ++)
-		a[i] = ((i)? a[i - 1] : 0) + ((s[i-1] == '(')? 1 : -1);
-	for(int i = 0 ; i <= n ; i ++)pos[a[i] + maxn].pb(i);
-	int st = 0;
-	stk[++st] = n+1;
-	for(int i = n ; i >= 0 ; i --){
-		while(st and a[i] <= a[stk[st]])st--;
-		if(st)R[i] = stk[st] - 1;
-		stk[++st]=i;
-	}
+	cin >> s;
+	SA(s);
+	LCP(s);
+	for(int i = 0 ; i < n ; i ++)
+		a[0][i] = ((i)? a[0][i - 1] : 0) + ((s[i] == '(')? 1 : -1) , pos[a[0][i]+maxn].pb(i);
+	for(int i = 0 ; i < Log ; i ++)
+		for(int j = 0 ; j < n ; j ++)
+			a[i+1][j] = min(a[i][j] , (j + (1 << i)>=n)?a[i][j]:a[i][j+(1 << i)]);
 	ll ans = 0;
 	for(int i = 0 ; i < n ; i ++){
 		if(s[sa[i]] == ')')break;
-		int l = sa[i] + lcp[i] + 1;
-		int r = R[sa[i]];
-		if(l > r)continue;
-		auto &v = pos[a[sa[i]] + maxn]; 
-		ans+=(upper_bound(v.begin() , v.end() , r) - 
-		 lower_bound(v.begin() , v.end() , l ) );
+		ans += solve(sa[i]) - solve(sa[i] , sa[i] + lcp[i] - 1);
 	}
 	cout << ans;
     return(0);
