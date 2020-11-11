c28df9d0f4ae72e75d095aac53025475ffa77938
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-11-11 22:15:00 +0330

    Add solution for problem `A - Stock Arbitraging` Link: https://codeforces.com/contest/1150/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1150/submission/98150728

---
+++ codeforces/1150/A.cpp
@@ -0,0 +1,48 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma,tune=native")
+//*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int  ,int > pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn  = 3e6;
+const ll mod =1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+int n , m , r;
+int a[maxn] , b[maxn];
+int ans ;
+
+int32_t main(){
+    migmig;
+	cin >> n >> m >> r;
+	ans = r;
+	for(int i = 0 ; i < n ; i ++)
+		cin >> a[i];
+	sort(a , a + n);
+	for(int i = 0 ; i < m ; i ++)
+		cin >> b[i];
+	sort(b ,b + m);
+	ans = max(ans , (r / a[0])*b[m-1] + r%a[0]);
+	cout << ans;
+
+
+
+    return(0);
+}
