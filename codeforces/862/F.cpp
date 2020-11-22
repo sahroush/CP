40353d98a07cb698e55bfef87db02e8d79d2c487
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-11-22 21:45:00 +0330

    Add solution for problem `F - Mahmoud and Ehab and the final stage` Link: https://codeforces.com/contest/862/problem/F Tags: data structures, strings, *2900 Ref: https://codeforces.com/contest/862/submission/99273465

--- codeforces/862/F.cpp
+++ codeforces/862/F.cpp
@@ -1,6 +1,6 @@
 //*
 #pragma GCC optimize("O2")
-#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("Ofast")
 #pragma GCC optimize("unroll-loops")
 //#pragma GCC target("avx,avx2,sse,sse2,fma")
 //*/
