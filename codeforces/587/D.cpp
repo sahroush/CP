057dd138ff619001a9ce89067fbab1366f5700c1
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-11-23 23:08:00 +0330

    Add solution for problem `D - Duff in Mafia` Link: https://codeforces.com/contest/587/problem/D Tags: 2-sat, binary search, *3100 Ref: https://codeforces.com/contest/587/submission/99349464

--- codeforces/587/D.cpp
+++ codeforces/587/D.cpp
@@ -25,35 +25,39 @@ const ld PI = acos((ld)-1);
 #define ms(x , y) memset(x , y , sizeof x)
 ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
 
-struct sat{
-	int n, c;
+struct sat{ //~x = 2*x+1 , x = 2*x --> ~x = x^1
+	int n;
 	vector < vector < int > > in , out;
-	vector < int > col , topo;
+	vector < int >  mark , col , topo;
+	int c = 0;
 	sat(int N):
-		n(N) , c(0) , in(2*n + 5) , out(2*n + 5) , col(2*n + 5){}
-	bool operator [] (int x) { return(col[2*x] > col[2*x + 1]); }
-	void add_e(int v , int u){in[u].pb(v) , out[v].pb(u);}
-	void add(int v , int u){add_e(u^1 , v) , add_e(v^1 , u);}
+		n(N) , in(n*2+5) , out(n*2+5) , mark(n*2+5 , 0) , col(n*2+5, 0){}
+	bool operator [] (int x){ return(col[2*x] > col[2*x + 1]);};
+	void add_edge(int v , int u){
+		in[u].pb(v) , out[v].pb(u);
+	}
+	void add(int u , int v){add_edge(v^1 , u) , add_edge(u^1 , v);}
 	void sfd(int v){
+		mark[v] = 1;
 		col[v] = c;
-		for(auto u : in[v]) if(!col[u])
+		for(int u : in[v]) if(!mark[u])
 			sfd(u);
 	}
 	void dfs(int v){
-		col[v] = 1;
-		for(auto u : out[v]) if(!col[u])
+		mark[v] = 1;
+		for(int u : out[v]) if(!mark[u])
 			dfs(u);
 		topo.pb(v);
 	}
-	bool validate(){
-		for(int i = 1 ; i <= 2*n+1 ; i ++ ) if(!col[i]) dfs(i);
+	inline bool validate(){
+		for(int i = 1 ; i <= 2*n + 1 ; i ++)if(!mark[i])dfs(i);
 		reverse(topo.begin() , topo.end());
-		fill(col.begin() , col.end() , 0 );
+		memset(&mark[0] , 0 , sizeof(mark[0]) * mark.size());
 		for(auto v : topo)
-			if(!col[v])
-				++c , sfd(v);
-		for(int i = 1 ; i <= n ; i ++) if(col[i * 2] == col[i * 2 + 1])return(0);
-	return(1);
+			if(!mark[v])
+				c++ , sfd(v);
+		for(int i = 1 ; i <= n ; i ++)if(col[2*i] == col[2*i+1])return(0);
+		return(1);
 	}
 };
 int n , m;
