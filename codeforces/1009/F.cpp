bb2267b301e5802ccafd09942e60797e324f1321
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-12-07 18:14:00 +0330

    Add solution for problem `F - Dominant Indices` Link: https://codeforces.com/contest/1009/problem/F Tags: data structures, dsu, trees, *2300 Ref: https://codeforces.com/contest/1009/submission/100636623

--- codeforces/1009/F.cpp
+++ codeforces/1009/F.cpp
@@ -1,6 +1,6 @@
-/*
+//*
 #pragma GCC optimize("O2")
-#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("Ofast")
 #pragma GCC optimize("unroll-loops")
 #pragma GCC target("avx,avx2,sse,sse2,fma")
 //*/
@@ -27,56 +27,27 @@ ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*
 
 int n;
 
-int cnt[maxn];
-int cur = 0;
+map < int , int > mp[maxn];
 int h[maxn] , ans[maxn];
-int sz[maxn];
-vector < int > adj[maxn] , Adj[maxn];
-int ord[maxn] , t = 0;
 
-bool mark[maxn];
-bool vis[maxn];
-vector < int > vec;
+vector < int > adj[maxn];
 
 void dfs(int v = 1 , int par = 0){
-	sz[v] = 1 , ord[++t] = v;
-	for(int u : Adj[v])if(u!=par)adj[v].pb(u) , h[u]=h[v]+1 , dfs(u , v) , sz[v] += sz[u];
+	for(int u : adj[v])if(u!=par)h[u]=h[v]+1 , dfs(u , v);
 }
 
-bool cmp(int a , int b){
-	return(sz[a] > sz[b]);
+void merge(int u , int v){
+	if(mp[u].size() > mp[v].size())swap(mp[u] , mp[v]) , ans[v] = ans[u];
+	for(auto [f , s] : mp[u]){
+		mp[v][f] += s;
+		if(mp[v][f] > mp[v][ans[v]])ans[v] = f;
+		if(mp[v][f] == mp[v][ans[v]])ans[v] = min(ans[v] , f);
+	}
 }
 
-void ins(int v){
-	cnt[h[v]]++;
-	if(cnt[h[v]] > cnt[cur])cur = h[v];
-	if(cnt[h[v]] == cnt[cur] and cur > h[v])cur = h[v];
-	if(!vis[h[v]])vis[h[v]] = 1 , vec.pb(h[v]);
-}
-
-void add(int v){
-	ins(v);
-	for(auto u : adj[v])
-		add(u);
-}
-
-void solve(int v){
-	if(mark[v])return;
-	mark[v] = 1;
-	auto &adj = ::adj[v];
-	int d = adj.size();
-	if(d == 0){ins(v) , ans[v] = cur;return;}
-	solve(adj[0]);
-	for(int i = 1 ; i < d ; i ++)
-		add(adj[i]);
-	ins(v);
-	ans[v] = cur;
-}
-
-void clean(){
-	for(int x : vec)
-		cnt[x] = 0 , vis[x] = 0;
-	vec.clear();
+void solve(int v = 1 , int par = 0){
+	mp[v][h[v]] = 1 , ans[v] = h[v];
+	for(auto u : adj[v])if(u!=par)solve(u , v) , merge(u , v);
 }
 
 int32_t main(){
@@ -84,11 +55,10 @@ int32_t main(){
 	cin >> n;
 	for(int i = 1 ; i < n ; i ++ ){
 		int u , v; cin >> u >> v;
-		Adj[u].pb(v) , Adj[v].pb(u);
+		adj[u].pb(v) , adj[v].pb(u);
 	}
 	dfs();
-	for(int i = 1 ; i <= n ; i ++)sort(adj[i].begin() , adj[i].end() , cmp);
-	for(int i = 1 ; i <= n ; i ++)solve(ord[i]) , clean();
+	solve();
 	for(int i = 1 ; i <= n ; i ++) cout << ans[i] - h[i] << endl;
     return(0);
 }
