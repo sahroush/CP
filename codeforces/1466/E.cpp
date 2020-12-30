d67a36a9983cbf17a593f62feb18aabcd2de7233
Soroush Sahraei <soroush.sahraei@gmail.com>
2020-12-30 23:33:00 +0330

    Add solution for problem `E - Apollo versus Pan` Link: https://codeforces.com/contest/1466/problem/E Tags: bitmasks, brute force, math, *1800 Ref: https://codeforces.com/contest/1466/submission/102864532

--- codeforces/1466/E.cpp
+++ codeforces/1466/E.cpp
@@ -1,33 +1,4 @@
-//*
-#pragma GCC optimize("O2")
-//#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC target("avx,avx2,sse,sse2,fma")
-//*/
 #include <bits/stdc++.h>
-
-#include <string>
-#include <bits/functexcept.h>
-#include <iosfwd>
-#include <bits/cxxabi_forced.h>
-#include <bits/functional_hash.h>
-
-#pragma push_macro("__SIZEOF_LONG__")
-#pragma push_macro("__cplusplus")
-#define __SIZEOF_LONG__ __SIZEOF_LONG_LONG__
-#define unsigned unsigned long
-#define __cplusplus 201102L
-
-#define __builtin_popcountl __builtin_popcountll
-#define __builtin_ctzl __builtin_ctzll
-
-#include <bitset>
-
-#pragma pop_macro("__cplusplus")
-#pragma pop_macro("__SIZEOF_LONG__")
-#undef unsigned
-#undef __builtin_popcountl
-#undef __builtin_ctzl
 
 using namespace std;
 
