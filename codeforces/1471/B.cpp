bbc6440335602f34a39dc24dda7a99dcc5ab3af4
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-01-05 18:20:00 +0330

    Add solution for problem `B - Strange List` Link: https://codeforces.com/contest/1471/problem/B Tags: brute force, greedy, implementation, math, *1100 Ref: https://codeforces.com/contest/1471/submission/103403258

---
+++ codeforces/1471/B.cpp
@@ -0,0 +1,63 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma")
+//*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll , ll> pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll maxn = 3e6;
+const ll mod = 1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+int T;
+ll a[maxn];
+ll n , x;
+queue < pii > q;
+
+
+
+int32_t main(){
+    migmig;
+	cin >> T;
+	while(T -- ){
+		cin >> n >> x;
+		while(!q.empty())q.pop();
+		for(int i = 0 ; i < n ; i ++)
+			cin >> a[i] , q.push(pii(a[i] , 1));
+		ll sum = 0;
+		while(1){
+			auto [a , t] = q.front();
+			if(a%x)break;
+			sum += a*t;
+			q.pop();
+			q.push(pii(a/x , t * x));
+		}
+		while(!q.empty()){
+			auto [a , t] = q.front();
+			q.pop();
+			sum += a*t;
+		}
+		cout << sum << endl;
+	}
+
+
+
+
+    return(0);
+}
