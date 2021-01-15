081e412979d470818a71f5c9beb010b5ad433235
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-01-15 15:38:00 +0330

    Add solution for problem `E - Apollo versus Pan` Link: https://codeforces.com/contest/1466/problem/E Tags: bitmasks, brute force, math, *1800 Ref: https://codeforces.com/contest/1466/submission/104407737

--- codeforces/1466/E.cpp
+++ codeforces/1466/E.cpp
@@ -1,9 +1,4 @@
-//*
-#pragma GCC optimize("O2")
-//#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC target("avx,avx2,sse,sse2,fma")
-//*/
+
 #include <bits/stdc++.h>
 
 #include <string>
