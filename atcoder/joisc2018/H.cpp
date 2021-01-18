e7756757b0bcc51e8d9e2f3873f33b8daa9a2be4
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-01-18 22:48:00 +0330

    Add solution for problem `H - ビ太郎のパーティー (Bitaro's Party)` Link: https://atcoder.jp/contests/joisc2018/tasks/joisc2018_h Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/joisc2018/submissions/19515501

--- atcoder/joisc2018/H.cpp
+++ atcoder/joisc2018/H.cpp
@@ -1,16 +1,14 @@
-//#pragma comment(linker, "/stack:200000000")
-//#pragma GCC optimize("Ofast")
-//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-typedef pair<int , int> pii;
-
-const int maxn = 1e5 + 10;
-const int sq = 100;
-const int inf = 2e9;
+const int MAXN=1e5+6;
+const int SQRT=110;
+const int INF=2e9;
+vector<int>g[MAXN];
+vector<int>rg[MAXN];
+vector<pair<int,int> >bestSQRT[MAXN];
+bool no[MAXN];
+int dist[MAXN],n,m,q;
+bool c[MAXN];
 
 #define pb push_back
 #define endl '\n'
@@ -18,86 +16,99 @@ const int inf = 2e9;
 #define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
 #define ms(x , y) memset(x , y , sizeof x)
 
-vector < int > adj[maxn], radj[maxn];
-vector < pii > vec[maxn];
-bool nt[maxn] , c[maxn];
-int n , m , q , d[maxn];
+typedef pair<int , int> pii;
 
-int solve(int v){
+int calc(int v){
 	for(int i = 1 ; i < v ; i ++)
-		d[i] = -inf;
-	d[v] = 0;
+		dist[i] = -INF;
+	dist[v] = 0;
 	for(int i = v - 1;  i >= 1 ; i --)
-		for(auto u : adj[i])
+		for(auto u : g[i])
 			if(u <= v)
-				d[i] = max(d[i] , d[u] + 1);
+				dist[i] = max(dist[i] , dist[u] + 1);
 	int ans = -1;
 	for(int i = 1 ; i <= v ; i ++)
-		if(!nt[i])
-			ans = max(ans , d[i]);
+		if(!no[i])
+			ans = max(ans , dist[i]);
 	return(ans);
 }
 
 vector < pii > vc;
-void prep(){
+void precompute(){
 	for(int i = 1 ; i <= n ; i ++){
 		vc.clear();
 		vc.pb({0 , i});
-		for(auto u : radj[i])
-			for(auto x : vec[u])
+		for(auto u : rg[i])
+			for(auto x : bestSQRT[u])
 				vc.pb({-x.first - 1 , x.second});
 		sort(vc.begin() , vc.end());
 		for(auto x : vc){
 			if(c[x.second])continue;
-			vec[i].pb({-x.first , x.second});
-			if(vec[i].size() == sq)break;
+			bestSQRT[i].pb({-x.first , x.second});
+			if(bestSQRT[i].size() == SQRT)break;
 			c[x.second] = 1;			
 		}
-		for(auto x : vec[i])
+		for(auto x : bestSQRT[i])
 			c[x.second] = 0;
 	}
 }
 
-int32_t main(){
-    migmig;
-	cin >> n >> m >> q;
-	for(int i = 0 ; i < m ; i ++){
-		int u , v;
-		cin >> u >> v;
-		adj[u].pb(v) , radj[v].pb(u);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin>>n>>m>>q;
+	for(int i=0;i<m;++i)
+	{
+		int x,y;
+		cin>>x>>y;
+		g[x].push_back(y);
+		rg[y].push_back(x);
 	}
-	prep();
-	for(int i =1 ; i <= q ; i ++){
-		int u , v;
-		cin >> u >> v;
-		vector < int > cur;
-		for(int j = 0 ; j < v ; j ++){
-			int x;
-			cin >> x;
-			cur.pb(x);
-			nt[x] = 1;
+	precompute();
+
+	for(int i=1;i<=q;++i)
+	{
+		int x,y;
+		cin>>x>>y;
+		vector<int>busy;
+		for(int j=0;j<y;++j)
+		{
+			int f;
+			cin>>f;
+			busy.push_back(f);
+			no[f]=1;
 		}
-		if(v >= sq){
-			cout << solve(u) << endl;
-			for(auto x : cur)
-				nt[x] = 0;
+		if(y>=SQRT)
+		{
+			cout<<calc(x)<<"\n";
+			for(auto f:busy)
+			{
+				no[f]=0;
+			}
 			continue;
 		}
-		int ans = -1;
-		for(auto x : vec[u]){
-			if(nt[x.second])continue;
-			ans = x.first;
-			break;
+		int ans=-1;
+		for(auto xd:bestSQRT[x])
+		{
+			if(no[xd.second])continue;
+			ans=xd.first;break;
 		}
-		if(ans == -1){
-			cout << solve(u) << endl;
-			for(int x : cur)
-				nt[x] = 0;
+		if(ans!=-1)
+		{
+			cout<<ans<<"\n";
+			for(auto f:busy)
+			{
+				no[f]=0;
+			}
 			continue;
 		}
-		cout << ans << endl;
-		for(int x : cur)
-			nt[x] = 0;
+		cout<<calc(x)<<"\n";
+		for(auto f:busy)
+		{
+			no[f]=0;
+		}
 	}
-    return(0);
+return 0;
 }
