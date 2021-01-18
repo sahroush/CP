150d48f145e90e8429dcd65846787cf4abaca0ca
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-01-18 22:52:00 +0330

    Add solution for problem `H - ビ太郎のパーティー (Bitaro's Party)` Link: https://atcoder.jp/contests/joisc2018/tasks/joisc2018_h Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/joisc2018/submissions/19515521

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-6 | [D - Stamp Rally](https://atcoder.jp/contests/agc002/tasks/agc002_d) | [C++ (GCC 9.2.1)](./atcoder/agc002/D.cpp) | `AtCoder` `*1000` | Apr/26/2021 22:25 | 
-5 | [C - Rabbit Exercise](https://atcoder.jp/contests/agc006/tasks/agc006_c) | [C++ (GCC 9.2.1)](./atcoder/agc006/C.cpp) | `AtCoder` `*800` | Apr/23/2021 15:21 | 
-4 | [C - Removing Coins](https://atcoder.jp/contests/agc033/tasks/agc033_c) | [C++ (GCC 9.2.1)](./atcoder/agc033/C.cpp) | `AtCoder` `*800` | Apr/20/2021 15:27 | 
-3 | [E - BBQ Hard](https://atcoder.jp/contests/agc001/tasks/agc001_e) | [C++ (GCC 9.2.1)](./atcoder/agc001/E.cpp) | `AtCoder` `*1400` | Apr/11/2021 18:20 | 
-2 | [D - Reversed LCS](https://atcoder.jp/contests/agc021/tasks/agc021_d) | [C++ (GCC 9.2.1)](./atcoder/agc021/D.cpp) | `AtCoder` `*900` | Feb/06/2021 15:16 | 
-1 | [E - Greedy Ant](https://atcoder.jp/contests/keyence2021/tasks/keyence2021_e) | [C++ (GCC 9.2.1)](./atcoder/keyence2021/E.cpp) | `AtCoder` `*700` | Jan/22/2021 11:08 | 
+7 | [D - Stamp Rally](https://atcoder.jp/contests/agc002/tasks/agc002_d) | [C++ (GCC 9.2.1)](./atcoder/agc002/D.cpp) | `AtCoder` `*1000` | Apr/26/2021 22:25 | 
+6 | [C - Rabbit Exercise](https://atcoder.jp/contests/agc006/tasks/agc006_c) | [C++ (GCC 9.2.1)](./atcoder/agc006/C.cpp) | `AtCoder` `*800` | Apr/23/2021 15:21 | 
+5 | [C - Removing Coins](https://atcoder.jp/contests/agc033/tasks/agc033_c) | [C++ (GCC 9.2.1)](./atcoder/agc033/C.cpp) | `AtCoder` `*800` | Apr/20/2021 15:27 | 
+4 | [E - BBQ Hard](https://atcoder.jp/contests/agc001/tasks/agc001_e) | [C++ (GCC 9.2.1)](./atcoder/agc001/E.cpp) | `AtCoder` `*1400` | Apr/11/2021 18:20 | 
+3 | [D - Reversed LCS](https://atcoder.jp/contests/agc021/tasks/agc021_d) | [C++ (GCC 9.2.1)](./atcoder/agc021/D.cpp) | `AtCoder` `*900` | Feb/06/2021 15:16 | 
+2 | [E - Greedy Ant](https://atcoder.jp/contests/keyence2021/tasks/keyence2021_e) | [C++ (GCC 9.2.1)](./atcoder/keyence2021/E.cpp) | `AtCoder` `*700` | Jan/22/2021 11:08 | 
+1 | [H - ビ太郎のパーティー (Bitaro's Party)](https://atcoder.jp/contests/joisc2018/tasks/joisc2018_h) | [C++ (GCC 9.2.1)](./atcoder/joisc2018/H.cpp) | `AtCoder` `*100` | Jan/18/2021 22:52 | 
--- atcoder/joisc2018/H.cpp
+++ atcoder/joisc2018/H.cpp
@@ -1,136 +1,103 @@
-#include<bits/stdc++.h>
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#include <bits/stdc++.h>
+
 using namespace std;
-const int MAXN=1e5+6;
-const int SQRT=110;
-const int INF=2e9;
-vector<int>g[MAXN];
-vector<int>rg[MAXN];
-vector<pair<int,int> >bestSQRT[MAXN];
-bool no[MAXN];
-int dist[MAXN],n,m,q;
-bool c[MAXN];
-/*bool cmp(pair<int,int> p1,pair<int,int> p2)
-{
-	if(p1.second!=p2.second)return p1.second>p2.second;
-	return p1.first<p2.first;
-}*/
-int calc(int u)
-{
-	for(int i=1;i<u;++i)
-	{
-		dist[i]=-INF;
-	}
-	dist[u]=0;
-	for(int i=u-1;i>=1;--i)
-	{
-		for(auto v:g[i])
-		{
-			if(v>u)continue;
-			dist[i]=max(dist[i],dist[v]+1);
-		}
-	}
-	int ret=-1;
-	for(int i=1;i<=u;++i)
-	{
-		if(no[i])continue;
-		ret=max(ret,dist[i]);
-	}
-	return ret;
+
+typedef long long ll;
+typedef pair<int , int> pii;
+
+const int maxn = 1e5 + 10;
+const int sq = 100;
+const int inf = 2e9;
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+
+vector < int > adj[maxn], radj[maxn];
+vector < pii > vec[maxn];
+bool nt[maxn] , c[maxn];
+int n , m , q , d[maxn];
+
+int solve(int v){
+	for(int i = 1 ; i < v ; i ++)
+		d[i] = -inf;
+	d[v] = 0;
+	for(int i = v - 1;  i >= 1 ; i --)
+		for(auto u : adj[i])
+			if(u <= v)
+				d[i] = max(d[i] , d[u] + 1);
+	int ans = -1;
+	for(int i = 1 ; i <= v ; i ++)
+		if(!nt[i])
+			ans = max(ans , d[i]);
+	return(ans);
 }
-vector<pair<int,int> >ivan;
-void precompute()
-{
-	for(int i=1;i<=n;++i)
-	{
-		ivan.clear();
-		ivan.push_back({0,i});
-		for(auto v:rg[i])
-		{
-			for(auto xd:bestSQRT[v])
-			{
-				ivan.push_back({-xd.first-1,xd.second});
-			}
+
+vector < pii > vc;
+void prep(){
+	for(int i = 1 ; i <= n ; i ++){
+		vc.clear();
+		vc.pb({0 , i});
+		for(auto u : radj[i])
+			for(auto x : vec[u])
+				vc.pb({-x.first - 1 , x.second});
+		sort(vc.begin() , vc.end());
+		for(auto x : vc){
+			if(c[x.second])continue;
+			vec[i].pb({-x.first , x.second});
+			if(vec[i].size() == sq)break;
+			c[x.second] = 1;			
 		}
-		sort(ivan.begin(),ivan.end());
-		for(auto xd:ivan)
-		{
-			if(c[xd.second])continue;
-			bestSQRT[i].push_back({-xd.first,xd.second});
-			if(bestSQRT[i].size()==SQRT)
-			{
-				break;
-			}
-			c[xd.second]=1;
-		}
-		for(auto xd:bestSQRT[i])
-		{
-			c[xd.second]=0;
-		}
+		for(auto x : vec[i])
+			c[x.second] = 0;
 	}
 }
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin>>n>>m>>q;
-	for(int i=0;i<m;++i)
-	{
-		int x,y;
-		cin>>x>>y;
-		g[x].push_back(y);
-		rg[y].push_back(x);
+
+int32_t main(){
+    migmig;
+	cin >> n >> m >> q;
+	for(int i = 0 ; i < m ; i ++){
+		int u , v;
+		cin >> u >> v;
+		adj[u].pb(v) , radj[v].pb(u);
 	}
-	precompute();
-	/*for(int i=1;i<=n;i++)
-	{
-		cout<<i<<":\n";
-		for(auto xd:bestSQRT[i])
-		{
-			cout<<xd.first<<" "<<xd.second<<endl;
+	prep();
+	for(int i =1 ; i <= q ; i ++){
+		int u , v;
+		cin >> u >> v;
+		vector < int > cur;
+		for(int j = 0 ; j < v ; j ++){
+			int x;
+			cin >> x;
+			cur.pb(x);
+			nt[x] = 1;
 		}
-	}*/
-	for(int i=1;i<=q;++i)
-	{
-		int x,y;
-		cin>>x>>y;
-		vector<int>busy;
-		for(int j=0;j<y;++j)
-		{
-			int f;
-			cin>>f;
-			busy.push_back(f);
-			no[f]=1;
-		}
-		if(y>=SQRT)
-		{
-			cout<<calc(x)<<"\n";
-			for(auto f:busy)
-			{
-				no[f]=0;
-			}
+		if(v >= sq){
+			cout << solve(u) << endl;
+			for(auto x : cur)
+				nt[x] = 0;
 			continue;
 		}
-		int ans=-1;
-		for(auto xd:bestSQRT[x])
-		{
-			if(no[xd.second])continue;
-			ans=xd.first;break;
+		int ans = -1;
+		for(auto x : vec[u]){
+			if(nt[x.second])continue;
+			ans = x.first;
+			break;
 		}
-		if(ans!=-1)
-		{
-			cout<<ans<<"\n";
-			for(auto f:busy)
-			{
-				no[f]=0;
-			}
+		if(ans == -1){
+			cout << solve(u) << endl;
+			for(int x : cur)
+				nt[x] = 0;
 			continue;
 		}
-		cout<<calc(x)<<"\n";
-		for(auto f:busy)
-		{
-			no[f]=0;
-		}
+		cout << ans << endl;
+		for(int x : cur)
+			nt[x] = 0;
 	}
-return 0;
+    return(0);
 }
