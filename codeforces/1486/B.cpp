ba0b9dc842e884241874004a1b122e4573f41dbf
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-02-19 13:49:00 +0330

    Add solution for problem `B - Eastern Exhibition` Link: https://codeforces.com/contest/1486/problem/B Tags: binary search, geometry, shortest paths, sortings, *1500 Ref: https://codeforces.com/contest/1486/submission/107931996

---
+++ codeforces/1486/B.cpp
@@ -0,0 +1,77 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma")
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int , int> pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int maxn = 3e6;
+const ll mod = 1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+int q;
+int n;
+int x[maxn] , y[maxn];
+
+int a[maxn];
+
+ll chk(ll x){
+	ll sum = 0;
+	for(int i = 1 ; i <= n ; i ++)
+		sum += abs(a[i] - x);
+	return(sum);
+}
+
+ll solve(int *a){
+	sort(a + 1 , a + n + 1);
+	for(int i = 1 ; i <= n ; i ++)::a[i] = a[i];
+	int med = a[(n + 1) / 2];
+	ll val = chk(med);
+	ll l = 0 , r = 1e11;
+	while(r - l > 1){
+		ll mid = (l + r) / 2;
+		if(chk(med + mid) == val)
+			l = mid;
+		else
+			r = mid;
+	}
+	ll R = med + l;
+	l = 0 , r = 1e11;
+	while(r - l > 1){
+		ll mid = (l + r) / 2;
+		if(chk(med - mid) == val)
+			l = mid;
+		else
+			r = mid;
+	}
+	ll L = med - l;
+	return(R - L + 1);
+}
+
+int32_t main(){
+	migmig;
+	cin >> q;
+	while(q -- ){
+		cin >> n;
+		for(int i = 1 ; i <= n ; i ++)
+			cin >> x[i] >> y[i];
+		cout << solve(x) * solve(y) << endl;
+	}
+	return(0);
+}
