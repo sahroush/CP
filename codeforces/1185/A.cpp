dc9fefeea6b3c6f864f3bd83b14be5d70400fa59
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-02-19 19:22:00 +0330

    Add solution for problem `A - Ropewalkers` Link: https://codeforces.com/contest/1185/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1185/submission/107959557

---
+++ codeforces/1185/A.cpp
@@ -0,0 +1,37 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma")
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int , int> pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int maxn = 3e6;
+const ll mod = 1e9+7;
+const ld PI = acos((ld)-1);
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+ll a[3] , d;
+
+int32_t main(){
+	migmig;
+	cin >> a[0] >> a[1] >> a[2] >> d;
+	sort(a , a + 3);
+	ll ans = max(0LL , d - abs(a[1] - a[0])) + max(0LL , d - abs(a[2] - a[1]));
+	cout << ans;
+	return(0);
+}
