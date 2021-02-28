104fd2d1ab0d8464a773dd71107b9c661593c9a4
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-02-28 14:14:00 +0330

    Add solution for problem `A - Broken Keyboard` Link: https://codeforces.com/contest/1251/problem/A Tags: brute force, strings, two pointers, *1000 Ref: https://codeforces.com/contest/1251/submission/108659299

--- codeforces/1251/A.cpp
+++ codeforces/1251/A.cpp
@@ -1,75 +1,63 @@
+/*
+#pragma GCC optimize("O2")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma")
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long ll;
 typedef long double ld;
+typedef pair<int , int> pii;
 
-const ll maxn = 1e5+10;
-const ll ZERO = 0;
-const ll SADAT = 7.84e-17;
-const ll INF = 1e9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int maxn = 3e6;
 const ll mod = 1e9+7;
+const ld PI = acos((ld)-1);
 
+#define pb push_back
 #define endl '\n'
-#define dokme(x) cout << x ;  return(0);
-#define migmig ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define file_init freopen("input.txt", "r+", stdin); freopen("output.txt", "w+", stdout);
+#define dokme(x) cout << x , exit(0)
+#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
 
-int n , q;
-string s ;
-char alp[26] = {'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' ,'t' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z'};
+int q;
+string s;
+int n;
+bool ok[30];
 
-int main(){
-    migmig;
-    cin >> q;
-    while (q--){
-        bool f[26];
-        pair <int , char> ans [1000];
-        fill (f , f+26 , 0);
-        cin >> s;
-        n = s.size();
-        string pos;
-        int sz = 0;
-        int i = 0;
-        while (i < n){
-            pos = s[i];
-            int cnt = 1;
+int32_t main(){
+	migmig;
+	cin >> q;
+	while(q -- ){
+		cin >> s;
+		n = s.size();
+		ms(ok , 0);
+		int lst = -1 , cnt = 0;
+		for(int i = 0 ; i < n ; i ++){
+			int c = s[i] - 'a';
+			if(lst == -1){
+				cnt = 1;
+				lst = c;
+				continue;
+			}
+			if(lst != c){
+				if(cnt % 2)ok[lst] = 1;
+				cnt = 0;
+				lst = c;
+			}
+			cnt++;
+		}
+		if(cnt % 2)ok[lst] = 1;
+		for(int i = 0 ; i < 27 ; i ++)
+			if(ok[i])
+				cout << char(i + 'a');
+		cout << endl;
+	}
 
-            while (i < n and s[i]==s[i+1] ){
-                cnt++;
-                i++;
-            }
-
-            ans[sz].first = cnt;
-            ans[sz].second= s[i];
-
-            sz++;
-            i++;
-        }
-        for (int i = 0; i  < sz ; i ++){
-            if (ans[i].first%2 == 1){
-                f[ans[i].second - 'a'] = 1;
-
-            }
-        }
-
-        for (int i = 0 ; i < 26 ; i ++){
-            if (f[i]){
-                cout << alp[i];
-            }
-        }
-        cout << endl;
-
-    }
-
-    return(0);
+	return(0);
 }
-
-
-
-
-//When we hit our lowest point, we are open to the greatest change.
-//If I try, I fail. If I don't try, I'm NEVER GOING TO GET IT.
-//Sometimes, life is like this dark tunnel. You can't always see the light at the end of the tunnel. But if you just keep moving... you will come to a better place.
-//It is important to draw wisdom from diffrent places. If you take it from only one place it becomes rigid and stale.
