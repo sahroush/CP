e9a9dc80b8c9e049470ea7bd5e1704a9a5256a88
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-03-29 22:52:00 +0430

    Add solution for problem `E - Two Houses` Link: https://codeforces.com/contest/1498/problem/E Tags: brute force, graphs, greedy, interactive, sortings, *2200 Ref: https://codeforces.com/contest/1498/submission/111412632

--- codeforces/1498/E.cpp
+++ codeforces/1498/E.cpp
@@ -1,63 +1,103 @@
-/*
-#pragma GCC optimize("O2")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC target("avx,avx2,sse,sse2,fma")
-*/
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
+#define FI first.first
+#define SE first.second
+#define TH second
+#define fi first
+#define se second
+#define th second
 
 using namespace std;
 
+string to_string(const string& str) {
+  return str;
+}
+
+template<typename T>
+string to_string(const set<T>& mys) {
+  if(mys.empty()) return "{}";
+  string ret = "{";
+  for(auto el: mys) ret += to_string(el) + ", ";
+  ret.pop_back(), ret.pop_back(); // Remove last space and comma
+  ret += "}";
+  return ret;
+}
+
+template<typename T>
+string to_string(const pair<T, T>& pr) {
+  return "(" + to_string(pr.fi) + "," + to_string(pr.se) + ")";
+}
+
+template<typename T>
+string to_string(const vector<T>& vc, int w) {
+  if(vc.empty()) return "";
+  if(w + 1 == vc.size()) return to_string(vc[w]);
+  return to_string(vc[w]) + "," + to_string(vc, w + 1);
+}
+
+template<typename T>
+string to_string(const vector<T>& vc) {
+  return "{" + to_string(vc, 0) + "}";
+}
+
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+ 
+#ifdef DEBUG
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+class DebugStream {}LOG;
+template <typename T>DebugStream &operator<<(DebugStream &s, const T&) { return s; }
+#ifdef DEBUG
+#define LOG clog
+#endif
+
 typedef long long ll;
+typedef pair<ll, ll> ii;
 typedef long double ld;
-typedef pair<int , int> pii;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int maxN = 1e5 + 9, maxV = 1e6 + 9, MOD = 1e9 + 7, SQ = 335, lg = 20, bs = 29;
 
-const int maxn = 510;
-const ll mod = 1e9+7;
-const ld PI = acos((ld)-1);
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  #ifdef DEBUG
+  //freopen("input.txt", "r", stdin);
+  #endif
 
-#define pb push_back
-#define endl '\n'
-#define dokme(x) cout << x , exit(0)
-#define migmig ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
-#define ms(x , y) memset(x , y , sizeof x)
-ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+  int n;
+  cin >> n;
+  vector<int> inc(n), inds(n);
+  iota(inds.begin(), inds.end(), 0);
+  for(auto& el: inc) cin >> el;
+  sort(inds.begin(), inds.end(), [&](int a, int b) {
+    return inc[a] < inc[b];
+  });
 
-int n;
-pii d[maxn];
-vector < int > vec[maxn];
-int ans = 0;
-pii mx = {1 , 1};
-
-int c2(int x){
-	return x * (x - 1) / 2;
+  for(int u = n - 1; u >= 0; u--) {
+    for(int i = 0; i < n; i++) {
+      for(int j = i + 1; j < n; j++) {
+        if(inc[inds[j]] - inc[inds[i]] != u) continue;
+        cout << "? " << inds[j] + 1 << ' ' << inds[i] + 1 << '\n';
+        cout.flush();
+        string resp;
+        cin >> resp;
+        if(resp == "Yes") {
+          cout << "! " << inds[i] + 1 << ' ' << inds[j] + 1 << '\n';
+          exit(0);
+        }
+      }
+    }
+  }
+  cout << "! 0 0\n";
 }
 
-int32_t main(){
-	migmig;
-	cin >> n;
-	for(int i = 1 ; i <= n ; i ++)
-		cin >> d[i].first , d[i].second = i;
-	sort(d + 1 , d + 1 + n);
-	int cur = 1;
-	int sum = 0;
-	for(int i = 1 ; i <= n ; i ++){
-		sum += d[i].first;
-		vec[cur].pb(i);
-		if(sum == c2(i))
-			cur++;
-	}
-	if(cur == n + 1)dokme("! 0 0");
-	for(int i = 1 ; i <= cur ; i ++)if(vec[i].size() >= 2){
-		int sz = vec[i].size();
-		for(int j = 0 ; j < sz ; j ++)
-			for(int k = j+1 ; k < sz ; k ++)
-				if(abs(d[vec[i][j]].first - d[vec[i][k]].first) >= ans)
-					ans = abs(d[vec[i][j]].first - d[vec[i][k]].first) , 
-					mx = {d[vec[i][j]].second , d[vec[i][k]].second};
-	}
-	cout << "! " << mx.first << ' ' << mx.second;
-	return(0);
-}
