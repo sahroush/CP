7a08d3cbb3c2e2ae3789ee0e26d8e389933a302c
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-04-18 12:00:00 +0430

    Add solution for problem `D - Kuro and GCD and XOR and SUM` Link: https://codeforces.com/contest/979/problem/D Tags: binary search, bitmasks, brute force, data structures, dp, dsu, greedy, math, number theory, strings, trees, *2200 Ref: https://codeforces.com/contest/979/submission/113389432

--- codeforces/979/D.cpp
+++ codeforces/979/D.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
-const int maxn = 1e5 + 10;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int , int> pii;
 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int maxn = 1e5 + 10;
+const ll mod = 1e9+7;
+
+#define pb push_back
 #define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
 
 int q , sz;
 vector < int > d[maxn];
@@ -36,16 +47,22 @@ int32_t main(){
 	cin >> q;
 	for(int i = 1 ; i < maxn ; trie[i] = new node() , i ++)
 		for(int j = i ; j < maxn ; j += i)
-			d[j].push_back(i);
+			d[j].pb(i);
 	while(q --){
-		int t , x , k , s;
+		int t , x;
 		cin >> t >> x;
 		if(t == 1 and !mark[x]){
 			mark[x] = 1;
 			for(auto y : d[x])trie[y]->add(x);
 		}
-		if(t == 2)
-			cin >> k >> s , cout << ((x % k) ? -1 : trie[k]->get(x , s-x)) << endl;
+		if(t == 2){
+			int k , s;
+			cin >> k >> s;
+			if(x % k)
+				cout << -1 << endl;
+			else
+				cout << trie[k]->get(x , s-x) << endl;
+		}
 	}
 	return(0);
 }
