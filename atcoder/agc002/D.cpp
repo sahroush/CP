3c354703e72265d00b4876a419f5983893714b38
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-04-26 22:25:00 +0430

    Add solution for problem `D - Stamp Rally` Link: https://atcoder.jp/contests/agc002/tasks/agc002_d Tags: AtCoder, *1000 Ref: https://atcoder.jp/contests/agc002/submissions/22099950

--- atcoder/agc002/D.cpp
+++ atcoder/agc002/D.cpp
@@ -9,7 +9,7 @@ typedef pair<int , int> pii;
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const int maxn = 1e5 + 10;
+const int maxn = 1e5+10;
 const ll mod = 1e9+7;
 
 #define pb push_back
@@ -18,45 +18,41 @@ const ll mod = 1e9+7;
 #define ms(x , y) memset(x , y , sizeof x)
 ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
 
-int n , m , q;
-pii e[maxn];
-int x[maxn] , y[maxn] , z[maxn] , l[maxn] , r[maxn] , par[maxn] , sz[maxn];
-vector < int > mid[maxn];
+int n , m , q , u , v , x , y , z;
+int par[maxn] , sz[maxn] , ti[maxn];
+vector < pii > hist[maxn];
 
 int getpar(int v){
-	return ((par[v]) ? par[v] = getpar(par[v]) : v);
-}
-
-void merge(int u , int v){
-	if((u = getpar(u)) == (v = getpar(v)))return;
-	par[u] = v;
-	sz[v] += sz[u];
+	return ((par[v]) ? getpar(par[v]) : v);
 }
 
 int32_t main(){
 	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
 	cin >> n >> m;
-	for(int i = 1 ; i <= m ; i ++)
-		cin >> e[i].first >> e[i].second;
-	cin >> q;
-	for(int i = 1  ; i <= q ; i ++)
-		cin >> x[i] >> y[i] >> z[i],
-		l[i] = 0 , r[i] = m;
-	for(int i = 0 ; i < 17 ; i ++){
-		for(int j = 1 ; j <= q ; j ++)	
-			mid[(l[j] + r[j]) >> 1].pb(j);
-		for(int j = 1 ; j <= n ; j ++)par[j] = 0 , sz[j] = 1;
-		for(int j = 1 ; j <= m ; j ++){
-			merge(e[j].first , e[j].second);
-			for(auto p : mid[j]){
-				if(getpar(x[p]) == getpar(y[p]))
-					((sz[getpar(x[p])] >= z[p]) ? r[p] = j : l[p] = j);
-				else
-					((sz[getpar(x[p])] + sz[getpar(y[p])] >= z[p]) ? r[p] = j : l[p] = j);
-			}
-			mid[j].clear();
-		}
+	for(int i = 1 ; i <= n ; i ++)sz[i] = 1 , hist[i].pb({0 , 1}) , ti[i] = m+1;
+	for(int i = 1 ; i <= m ; i ++){
+		cin >> u >> v;
+		if((u = getpar(u)) == (v = getpar(v)))continue;
+		if(sz[v] < sz[u])swap(u , v);
+		sz[v] += sz[u] , par[u] = v, ti[u] = i;
+		hist[v].pb({i , sz[v]});
 	}
-	for(int i = 1 ; i <= q ; i ++)cout << r[i] << endl;
+	cin >> q;
+	while(q --){
+		cin >> u >> v >> z;
+		int l = 0 , r = m;
+		while(r - l > 1){
+			int mid = (l + r) / 2;
+			x = u , y = v;
+			while(ti[x] <= mid) x = par[x];
+			while(ti[y] <= mid) y = par[y];
+			int val = prev(lower_bound(hist[x].begin() , hist[x].end() , pii(mid , m+1)))->second;
+			if(x ^ y)
+				val+= prev(lower_bound(hist[y].begin() , hist[y].end() , pii(mid , m+1)))->second;
+			if(val >= z)r = mid;
+			else l = mid;
+		}
+		cout << r << endl;
+	}
 	return(0);
 }
