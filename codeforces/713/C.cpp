6bb86d9787a46883b113e9f498593655a93214e0
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-05-26 17:06:00 +0430

    Add solution for problem `C - Sonya and Problem Wihtout a Legend` Link: https://codeforces.com/contest/713/problem/C Tags: dp, sortings, *2300 Ref: https://codeforces.com/contest/713/submission/117406894

--- codeforces/713/C.cpp
+++ codeforces/713/C.cpp
@@ -1,18 +1,34 @@
+//叫んで 藻掻もがいて 瞼まぶたを腫らしても
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int n , a[3010];
-int64_t ans;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll , ll> pii;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int maxn = 3010;
+const ll mod = 1e9+7;
+
+#define pb push_back
+#define endl '\n'
+#define dokme(x) cout << x , exit(0)
+#define ms(x , y) memset(x , y , sizeof x)
+ll pw(ll a, ll b, ll md = mod){ll res = 1;while(b){if(b&1){res=(a*res)%md;}a=(a*a)%md;b>>=1;}return(res);}
+
+int n , a[maxn];
+ll ans;
 priority_queue < int > pq;
 
 int32_t main(){
 	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
 	cin >> n, pq.push(-1);
-	for(int i = 1 ; i <= n ; pq.push(a[i]) , i ++){
+	for(int i = 1 ; i <= n ; i ++)
 		cin >> a[i] , a[i] -= i , a[i] += n;
-		if(a[i] < pq.top()) ans += pq.top() , pq.pop() , pq.push(a[i]) , ans -= a[i];
-	}
+	for(int i = 1 ; i <= n ; pq.push(a[i]), i ++) if(a[i] < pq.top())
+			ans += pq.top() , pq.pop() , pq.push(a[i]) , ans -= a[i];
 	cout << ans;
 	return(0);
 }
