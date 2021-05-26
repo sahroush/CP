ce202e1dacdc8ca406e81cc887135b207a5c5e9b
Soroush Sahraei <soroush.sahraei@gmail.com>
2021-05-26 17:11:00 +0430

    Add solution for problem `C - Sonya and Problem Wihtout a Legend` Link: https://codeforces.com/contest/713/problem/C Tags: dp, sortings, *2300 Ref: https://codeforces.com/contest/713/submission/117407406

--- codeforces/713/C.cpp
+++ codeforces/713/C.cpp
@@ -1,2 +1,18 @@
 #include <bits/stdc++.h>
-using namespace std;int64_t ans,n,a[3010];priority_queue<int>pq;int main(){cin>>n,pq.push(-1);for(int i=1;i<=n;pq.push(a[i]),i++){cin>>a[i],a[i]+=n-i;if(a[i] < pq.top())ans+=pq.top(),pq.pop(),pq.push(a[i]),ans-=a[i];}cout<<ans;}
+
+using namespace std;
+
+int n , a[3010];
+int64_t ans;
+priority_queue < int > pq;
+
+int32_t main(){
+	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+	cin >> n, pq.push(-1);
+	for(int i = 1 ; i <= n ; pq.push(a[i]) , i ++){
+		cin >> a[i] , a[i] -= i , a[i] += n;
+		if(a[i] < pq.top()) ans += pq.top() , pq.pop() , pq.push(a[i]) , ans -= a[i];
+	}
+	cout << ans;
+	return(0);
+}
